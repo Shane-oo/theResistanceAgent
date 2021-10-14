6bcc47a6e26ce65fee8cd0e8a86abaf0578bf98e
Shane <shanemonck11@hotmail.com>
2021-10-14 20:02:34 +0800

    bigger games

--- resistanceAI-master/src-py/resistance/__main__.py
+++ resistanceAI-master/src-py/resistance/__main__.py
@@ -1,16 +1,20 @@
-from random_agent import RandomAgent
-from myAgent22501734 import logicalAgent
 from stupidAgents import StupidAgent
+from myAgent22501734 import logicalAgent
 from game import Game
 allData =[]
 spyWinCount = 0
 resitanceWinCount=0
-for i in range(5000):
+for i in range(15000):
         agents = [logicalAgent(name='r1'), 
                 logicalAgent(name='r2'),  
                 logicalAgent(name='r3'),  
-                StupidAgent(name='r4'),  
-                StupidAgent(name='r5'),  ]
+                logicalAgent(name='r4'),  
+                logicalAgent(name='r5'),  
+                logicalAgent(name='r6'),  
+                StupidAgent(name='r7'),  
+                StupidAgent(name='r8'),  
+                StupidAgent(name='r9'),  
+                StupidAgent(name='r10'),  ]
         
         game = Game(agents)
         game.play()
@@ -20,25 +24,30 @@ for i in range(5000):
         spy_list = []
         for agent in agents:
                 # resistance player
-                if(index == 0 or index==1 or index==2):
+                if(index == 0 or index==1 or index==2 or index==3 or index ==4 or index ==5 ) :
                         if(agent.returnValues(index)[0] == 1):
                                 resistanceData.append((agent.returnValues(index)[1]))
                         if(agent.whoWon()!=-1):
                                 whoWonValue = agent.whoWon()
                 else:
-
                         spy_list = (agent.returnValues(index)[1])
                         print("THE SPY LIST",spy_list)
+                        if(spy_list!=-1):
+                                spy_list.sort()
+                        
                                 
                
                 
                 index +=1
-        if (spy_list != [3,4]):
+        if (spy_list != [6,7,8,9]):
                 print(resistanceData)
                 print("OH NO")
                 continue
-        resistanceData[0][3][10] = 1
-        resistanceData[0][4][10] = 1
+        resistanceData[0][6][10] = 1
+        resistanceData[0][7][10] = 1
+        resistanceData[0][8][10] = 1
+        resistanceData[0][9][10] = 1
+
         if(whoWonValue == True ):
                 resitanceWinCount+=1
         else:
@@ -53,6 +62,7 @@ for i in range(5000):
                                 count +=1
                 index+=1        
 
+        i = 0
         for  i in range(len(resistanceData)+1):
                 if (i in spy_list):
                         resistanceData[0][i][10] = 1
